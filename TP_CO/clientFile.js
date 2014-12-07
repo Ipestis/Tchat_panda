f66a5bce544fbd56e6aa457fcebf11b672d11dc1
Ipestis <p.arthur92@gmail.com>
2014-12-07 20:47:54 +0100

    FINAL

--- TP_CO/clientFile.js
+++ TP_CO/clientFile.js
@@ -1,35 +1,49 @@
 var util = require('util');
 var net = require('net');
 var fs = require('fs');
+require("./styles.js").add_theme();
 
-
-
+//Connexion du client au serveur
 var client = net.connect({port: 8124},
     	function() { //'connect' listener
-  	console.log('client connected');
+	console.log('\tWelcome to the server !'.magenta.bold);
+	console.log("You are connected\n".blue.bold);
 });
 
 process.stdin.setEncoding('utf8');
-console.log("Fichier a envoyer: ");
+
+//Gestion des entrées du client
 process.stdin.on('data', function(data) {
 	var fichier = data.toString().trim();
+	
+	//Envoie du nom du fichier en cas d'utilisation du fileName
 	if(fichier.indexOf("fileName=")==0 && fichier.length>9){
-		console.log("Changing file name to "+fichier.substring(9));
-		client.write(fichier);
+		//console.log("Changing file name to "+fichier.substring(9));
+		client.write(fichier+".txt");
 	}
 	else{
+		//Test de l'existe du nom de fichier donné
 		fs.exists(fichier, function(exists) {
 			if(exists){
+				//Création, lecture et envoie du fichier
 				var readstream = fs.createReadStream(fichier);
 				readstream.pipe(client, {end:false});
-				/*readstream.on("data", function(data){
-					client.write(data);
-				});*/
+				util.log("File send !".green);
 			}
-			else console.log("Le fichier "+fichier+" n'existe pas");
+			else{
+				console.log("The file ".red+fichier+" doesn't exist".red);
+				console.log("\nFile to send: ".cyan.bold+"(To give a file name: fileName=XX)".cyan.italic);
+			}
 		});
 	}
 });
+
+//Affichage des messages envoyés par le serveur
+client.on('data', function(data) {
+	console.log(data.toString());
+});
+
+//Gestion de la déconnection
 client.on("end", function(){
 	console.log("Disconnected");
 });
--- TP_CO/server.js
+++ TP_CO/server.js
@@ -1,30 +1,40 @@
 var net = require('net');
 var fs = require('fs');
+require("./styles.js").add_theme();
 var clients = [];
 
+//Création du serveur
 var server = net.createServer(function(socket) {
-  	
-
+	
+	//Message en cas de déconnection
   	socket.on('end', function() {
     		console.log('Client disconnected');
   	});
+	
+	//Definition des paramètres des sockets
 	socket.name = socket.remoteAddress+":"+socket.remotePort;
-	socket.fileName="defaultfileName.txt";
+	socket.fileName="defaultFileName.txt";
 	socket.setEncoding('utf8');
 	clients.push(socket);
-	console.log('Client connected :'+socket.name);
+	
+	//Connexion d'un client
+	console.log("Client connected : "+socket.name);
+	socket.write("File to send: ".cyan.bold+"(To give a file name: fileName=XX)".cyan.italic);
+	
+	//Gestion des données recues
 	socket.on('data',function(data){
+		//En cas de definiton d'un fileName: on renomme le fileName de la socket et on demande le fichier à l'utilisateur
 		if(data.indexOf("fileName=")==0){
 			socket.fileName=data.substring(9);
-			console.log("file name changed to "+socket.fileName);
+			console.log("File name changed to "+socket.fileName);
+			socket.write("File: ".cyan.bold);
 		}
 		else{
 			var writer = fs.createWriteStream(socket.fileName);
-			writer.write(data);
+			writer.write(data); //Ajout des données dans le fichier
+			socket.write("\nFile to send: ".cyan.bold+"(To give a file name: fileName=XX)".cyan.italic);
 		}
-		//console.log(data);
 	})
-
 });
 
 server.listen(8124, function() { 
